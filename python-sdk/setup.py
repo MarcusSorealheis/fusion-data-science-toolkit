61aae25814cbb38158113f15e9dd529bd8413041
Andy Liu <andy.liu@lucidworks.com>
2019-09-09 11:41:57 -0400

    Update requirements.txt

--- python-sdk/setup.py
+++ python-sdk/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='lucidworks-ml-toolkit',
+    name='lucidworks-data-science-integration-toolkit',
     version='0.1',
     packages=find_packages(),
     install_requires=[
